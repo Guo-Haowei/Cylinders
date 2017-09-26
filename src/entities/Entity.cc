de34ddaf06117a704ef47085ca6af60b9e753470
Guo-Haowei <ghwhotline@gmail.com>
2017-09-26 12:47:22 -0400

    Use mat4 for rotation instead of vec3

--- src/entities/Entity.cc
+++ src/entities/Entity.cc
@@ -1,18 +1,14 @@
 #include "Entity.h"
 #include <glm/gtc/matrix_transform.hpp>
 
-Entity::Entity(RawModel& model, glm::vec3 color, glm::vec3 pos, glm::vec3 rot, glm::vec3 scale): model(model), color(color), pos(pos), rot(rot), scale(scale) { }
+Entity::Entity(RawModel& model, glm::vec3 color, glm::vec3 pos, glm::vec3 scale): model(model), color(color), pos(pos), scale(scale) {
+  rotationMatrix = glm::mat4(1);
+}
 
 void Entity::changePosition(float dx, float dy, float dz) {
   pos.x += dx;
   pos.y += dy;
   pos.z += dz;
-}
-
-void Entity::changeRotation(float dx, float dy, float dz) {
-  rot.x += (dx);
-  rot.y += (dy);
-  rot.z += (dz);
 }
 
 RawModel& Entity::getModel() const {
@@ -21,10 +17,6 @@ RawModel& Entity::getModel() const {
 
 glm::vec3 Entity::getPos() const {
   return pos;
-}
-
-glm::vec3 Entity::getRot() const {
-  return rot;
 }
 
 glm::vec3 Entity::getScale() const {
@@ -39,10 +31,6 @@ void Entity::setPos(glm::vec3 pos) {
   this->pos = pos;
 }
 
-void Entity::setRot(glm::vec3 rot) {
-  this->rot = rot;
-}
-
 void Entity::setScale(glm::vec3 scale) {
   this->scale = scale;
 }
@@ -53,10 +41,11 @@ void Entity::setColor(glm::vec3 color) {
 
 glm::mat4 Entity::createTransformationMatrix() {
   glm::mat4 matrix = glm::mat4(1);
+  // rotate
+  matrix = rotationMatrix * matrix;
+  // translate
   matrix = glm::translate(matrix, pos);
-  matrix = glm::rotate(matrix, rot.x, glm::vec3(1, 0, 0));
-  matrix = glm::rotate(matrix, rot.y, glm::vec3(0, 1, 0));
-  matrix = glm::rotate(matrix, rot.z, glm::vec3(0, 0, 1));
+  // scale
   matrix = glm::scale(matrix, scale);
   return matrix;
 }
--- src/entities/Entity.h
+++ src/entities/Entity.h
@@ -6,22 +6,18 @@ class Entity {
 private:
   RawModel& model;
   glm::vec3 pos;
-  glm::vec3 rot;
   glm::vec3 scale;
-
+  glm::mat4 rotationMatrix;
   glm::vec3 color;
 public:
-  Entity(RawModel& model, glm::vec3 color, glm::vec3 pos = glm::vec3(0, 0, 0), glm::vec3 rot = glm::vec3(0, 0, 0), glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0));
+  Entity(RawModel& model, glm::vec3 color, glm::vec3 pos = glm::vec3(0, 0, 0), glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0));
   void changePosition(float dx, float dy, float dz);
-  void changeRotation(float dx, float dy, float dz);
 
   RawModel& getModel() const;
   glm::vec3 getPos() const;
-  glm::vec3 getRot() const;
   glm::vec3 getScale() const;
   glm::vec3 getColor() const;
   void setPos(glm::vec3 pos);
-  void setRot(glm::vec3 rot);
   void setScale(glm::vec3 scale);
   void setColor(glm::vec3 color);
   glm::mat4 createTransformationMatrix();
