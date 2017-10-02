35a9c288e304a1ad615b231b847147983a4852e1
Guo-Haowei <ghwhotline@gmail.com>
2017-10-02 19:49:31 -0400

    Compile all files as cpp

--- src/cyl/geometry.cc
+++ src/cyl/geometry.cc
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <math.h>
 #include "geometry.h"
 
 /* Convenient macros */
--- src/cyl/matrix.cc
+++ src/cyl/matrix.cc
@@ -15,6 +15,7 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "matrix.h"
 
 
