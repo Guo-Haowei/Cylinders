efbd114b0521c845c9d99ebeaa14f5300e174f88
Guo-Haowei <ghwhotline@gmail.com>
2017-10-02 21:32:48 -0400

    Add colors

--- src/common.h
+++ src/common.h
@@ -1,4 +1,5 @@
 #pragma once
+
 extern const int WINDOW_WIDTH;
 extern const int WINDOW_HEIGHT;
 extern int WIDTH;
@@ -7,3 +8,16 @@ extern int HEIGHT;
 const float ZOOM = 45.0f;
 const float NEAR_PLANE = 0.1f;
 const float FAR_PLANE = 1000.0f;
+
+const float COLORS[] = {
+  0.3f, 0.4f, 0.3f,
+  0.3f, 0.4f, 0.5f,
+  0.1f, 0.6f, 0.5f,
+  0.5f, 0.2f, 0.2f,
+  0.3f, 0.6f, 0.1f,
+  0.4f, 0.5f, 0.3f,
+  0.8f, 0.3f, 0.2f,
+  0.2f, 0.8f, 0.5f,
+  0.7f, 0.5f, 0.7f,
+  0.6f, 0.3f, 0.1f,
+};
