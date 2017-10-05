4b3aa6e9456b621b11165e19506e9a35fbcc3698
Guo-Haowei <ghwhotline@gmail.com>
2017-10-05 10:01:52 -0400

    Add world rotation

--- src/entities/Entity.h
+++ src/entities/Entity.h
@@ -8,9 +8,11 @@ private:
   glm::vec3 pos;
   glm::vec3 scale;
   glm::mat4 rotationMatrix;
+  glm::vec3 rotationCenter;
+  glm::mat4 rotationCenterMatrix;
   glm::vec3 color;
 public:
-  Entity(RawModel& model, glm::vec3 color, glm::vec3 pos = glm::vec3(0, 0, 0), glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0), glm::mat4 rotationMatrix = glm::mat4(1.0f));
+  Entity(RawModel& model, glm::vec3 color, glm::vec3 pos = glm::vec3(0, 0, 0), glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0), glm::mat4 rotationMatrix = glm::mat4(1.0f), glm::mat4 rotationCenterMatrix = glm::mat4(1.0f));
   void changePosition(float dx, float dy, float dz);
 
   RawModel& getModel() const;
@@ -22,6 +24,8 @@ public:
   void setPos(glm::vec3 pos);
   void setScale(glm::vec3 scale);
   void setColor(glm::vec3 color);
+  void setRotationCenter(glm::vec3 center);
+  void setRotationCenterMatrix(glm::mat4 mat);
   glm::mat4 createTransformationMatrix();
 
   static RawModel createUniformCylinder();
