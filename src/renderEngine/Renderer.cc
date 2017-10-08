5e43b50b940a6f1832515d5f570edd237a732038
Guo-Haowei <ghwhotline@gmail.com>
2017-10-08 16:52:49 -0400

    Implement color picking

--- src/renderEngine/Renderer.cc
+++ src/renderEngine/Renderer.cc
@@ -1,33 +1,81 @@
 #define GLEW_STATIC
 #include "Renderer.h"
 #include "../common.h"
+#include "../inputManager/MouseManager.h"
+#include "../models/CylinderList.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <GL/glew.h>
 #include <iostream>
 
 Renderer::Renderer() { }
 
+Renderer::~Renderer() {
+  entityShader.clean();
+  colorPickShader.clean();
+}
+
 void Renderer::render(vector<Entity*> entities) {
   prepare();
+
+  colorPickShader.start();
+  colorPickShader.loadViewMatrix();
+  colorPickShader.loadProjectionMatrix(glm::perspective(ZOOM, (float) WIDTH / (float) HEIGHT, NEAR_PLANE, FAR_PLANE));
+  for (int i = 0; i < entities.size(); ++i) {
+    prepareModel(entities[i]);
+    renderEntity(entities[i], COLORPICK);
+  }
+  colorPickShader.stop();
+
+  if (MouseManager::buttonPressed(0)) {
+    glFlush();
+    glFinish();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    unsigned char data[4];
+    int x = 2 * (int)MouseManager::currentX;
+    int y = HEIGHT - 2 * (int)MouseManager::currentY;
+    glReadPixels(x, y, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+    int id = data[0] + data[1] * 256 + data[2] * 256 * 256;
+    std::cout << "select " << id << "\n";
+    if (id == 0x00ffffff) {
+      CylinderList::selected = nullptr;
+      MouseManager::setMode(SCENE);
+    } else {
+      for (int i = 0; i < CylinderList::cylinders.size(); ++i) {
+        if (CylinderList::cylinders[i]->getID() == id) {
+          CylinderList::selected = CylinderList::cylinders[i];
+          MouseManager::setMode(OBJECT);
+          break;
+        }
+      }
+    }
+  }
+
+  prepare();
+
   entityShader.start();
   entityShader.loadViewMatrix();
   entityShader.loadProjectionMatrix(glm::perspective(ZOOM, (float) WIDTH / (float) HEIGHT, NEAR_PLANE, FAR_PLANE));
-  for (auto& entity: entities) {
-    prepareModel(entity);
-    renderEntity(entity);
+  for (int i = 0; i < entities.size(); ++i) {
+    prepareModel(entities[i]);
+    renderEntity(entities[i]);
   }
   entityShader.stop();
 }
 
-void Renderer::renderEntity(Entity* entity) {
-  entityShader.loadColor(entity->getColor());
-  entityShader.loadTransformationMatrx(entity->createTransformationMatrix());
-  // glDrawElements(GL_TRIANGLES, entity->getModel().getVertexCount(), GL_UNSIGNED_INT, (void*) 0);
+void Renderer::renderEntity(Entity* entity, SHADER type) {
+  if (type == ENTITY) {
+    entityShader.loadColor(entity->getColor());
+    entityShader.loadTransformationMatrx(entity->createTransformationMatrix());
+  } else if (type == COLORPICK) {
+    colorPickShader.loadColor(entity->getID());
+    colorPickShader.loadTransformationMatrx(entity->createTransformationMatrix());
+  }
   glDrawArrays(GL_TRIANGLES, 0, entity->getModel().getVertexCount());
 }
 
 void Renderer::prepare() {
-  glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
--- src/renderEngine/Renderer.h
+++ src/renderEngine/Renderer.h
@@ -2,17 +2,25 @@
 #include "../entities/Entity.h"
 #include "../models/RawModel.h"
 #include "../shaders/EntityShader.h"
+#include "../shaders/ColorPickShader.h"
 #include <vector>
 using std::vector;
+
+enum SHADER {
+  ENTITY,
+  COLORPICK
+};
 
 class Renderer {
 private:
   EntityShader entityShader;
+  ColorPickShader colorPickShader;
 public:
   Renderer();
+  ~Renderer();
 
   void render(vector<Entity*> entities);
-  void renderEntity(Entity* entity);
+  void renderEntity(Entity* entity, SHADER type = ENTITY);
   void prepare();
   void prepareModel(Entity* entity);
   void unbindModel();
